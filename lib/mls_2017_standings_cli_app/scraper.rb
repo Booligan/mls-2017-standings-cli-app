5b5be0d92a385dbf2e4930a035abd211df3ac059
Alberto Mayanga <amayanga90@yahoo.com>
2017-10-16 02:00:47 +0000

    scraped team name to hash array

--- lib/mls_2017_standings_cli_app/scraper.rb
+++ lib/mls_2017_standings_cli_app/scraper.rb
@@ -1,7 +1,33 @@
 class Mls2017StandingsCliApp::Scraper
-  def scrape_mls_teams
+
+  def self.scrape_mls_teams
     html = open("https://www.mlssoccer.com/standings")
     doc = Nokogiri::HTML(html)
+    table = doc.css('.standings_table a')
+    teams = []
+    teams_hash_array = []
+
+    table.each do |team|
+        teams << team.text
+    end
+
+    teams.delete("PPG")
+    teams.delete("GP")
+    teams.delete("W")
+    teams.delete("L")
+    teams.delete("T")
+    teams.delete("GF")
+    teams.delete("GA")
+    teams.delete("GD")
+    teams.delete("")
+
+    teams.each do|team|
+      team_hash = {:name => team}
+      teams_hash_array << team_hash
+    end
+    teams_hash_array
     binding.pry
-  end
+ end
+
+
 end
