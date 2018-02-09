30125ef8c52bf75ce6c88cedfc9253cbb22cef8a
alberto <github email address>
2018-02-09 17:13:29 -0500

    can now run gem using only 'mls_standings'

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,6 @@ require 'nokogiri'
 require 'colorize'
 
 
-require_relative '../lib/mls_standings/scraper'
+require_relative '../lib/mls_standings/scrapper'
 require_relative '../lib/mls_standings/team'
 require_relative '../lib/mls_standings/cli'
--- lib/mls_standings/version.rb
+++ lib/mls_standings/version.rb
@@ -1,3 +1,3 @@
 module MlsStandings
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
